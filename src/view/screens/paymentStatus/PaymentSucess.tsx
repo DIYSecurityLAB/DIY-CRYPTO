d3692ff33c31453a394971732b08d74b08c8c5b9
jefflds <jeffersonlds3009@gmail.com>
2025-01-13 11:31:44 +0000

    feat: replace svg to react icon

--- src/view/screens/paymentStatus/PaymentSucess.tsx
+++ src/view/screens/paymentStatus/PaymentSucess.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { FaCheckCircle } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
 export function PaymentSuccess() {
@@ -8,20 +9,7 @@ export function PaymentSuccess() {
   return (
     <div className="min-h-screen bg-primary-light dark:bg-primary-dark flex flex-col justify-center items-center px-4 text-center">
       <div className="bg-success rounded-full p-8 mb-8">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-24 w-24 text-white"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M5 13l4 4L19 7"
-          />
-        </svg>
+        <FaCheckCircle className="h-24 w-24 text-white" />
       </div>
       <h1 className="text-4xl md:text-5xl font-bold text-success dark:text-white mb-6">
         {t('paymentSuccess.title')}

94c4199e3c6e51ef8605cdf19a28d5591d89a2dc
Lucasqc04 <lucasqcampos9@gmail.com>
2025-01-28 13:23:45 -0300

    feat(checkout): add cpf validation

--- src/view/screens/Checkout/PersonForm/usePersonForm.ts
+++ src/view/screens/Checkout/PersonForm/usePersonForm.ts
@@ -3,6 +3,28 @@ import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { GetCheckout } from '../../../../domain/entities/payment.entity';
 
+// Função para validar CPF
+const isValidCPF = (cpf: string): boolean => {
+  cpf = cpf.replace(/\D/g, '');
+  if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
+
+  let sum = 0;
+  let rest;
+
+  for (let i = 0; i < 9; i++) sum += parseInt(cpf.charAt(i)) * (10 - i);
+  rest = (sum * 10) % 11;
+  if (rest === 10 || rest === 11) rest = 0;
+  if (rest !== parseInt(cpf.charAt(9))) return false;
+
+  sum = 0;
+
+  for (let i = 0; i < 10; i++) sum += parseInt(cpf.charAt(i)) * (11 - i);
+  rest = (sum * 10) % 11;
+  if (rest === 10 || rest === 11) rest = 0;
+
+  return rest === parseInt(cpf.charAt(10));
+};
+
 export function usePersonForm() {
   const { t } = useTranslation();
   const form = useFormContext<GetCheckout>();
@@ -39,5 +61,6 @@ export function usePersonForm() {
     form,
     whatsappLink,
     birthdayMask,
+    isValidCPF,
   };
 }

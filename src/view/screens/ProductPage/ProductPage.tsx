cc5fb16db076d1e4be8f2c5dff151216817315c9
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2025-01-31 02:24:31 +0000

    feat(product-page): solve responsiveness

--- src/view/screens/ProductPage/ProductPage.tsx
+++ src/view/screens/ProductPage/ProductPage.tsx
@@ -1,18 +1,22 @@
-import { AcceptedLanguages, LanguageTexts, useLanguage } from '@/domain/locales/Language';
+import {
+  AcceptedLanguages,
+  LanguageTexts,
+  useLanguage,
+} from '@/domain/locales/Language';
 import { BackgroundAnimatedProduct } from '@/view/components/BackgroundAnimatedProduct';
 import { Loader } from '@/view/components/Loader';
 import { useScaleFactor } from '@/view/hooks/useScaleFactor';
+import { ROUTES } from '@/view/routes/Routes';
 import { useWindowSize } from '@/view/utils/useWindowSize';
 import classNames from 'classnames';
 import { useState } from 'react';
-import { MdCheck, MdExpandMore, MdExpandLess } from 'react-icons/md';
+import { MdCheck, MdExpandLess, MdExpandMore } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import './product-page.css';
 import { useProductPage } from './useProductPage';
-import { Link } from 'react-router-dom';
-import { ROUTES } from '@/view/routes/Routes';
 
 export function ProductPage() {
   const { t, cart, product, quantity } = useProductPage();
@@ -22,7 +26,7 @@ export function ProductPage() {
 
   const [mainImage, setMainImage] = useState(product?.images[0]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isExpanded, setIsExpanded] = useState(false); // Estado para controlar a expansão da caixa de recursos
+  const [isExpanded, setIsExpanded] = useState(false);
 
   if (!product) {
     return <Loader />;
@@ -51,7 +55,7 @@ export function ProductPage() {
     : 0;
 
   const replaceAlfredP2P = (text: string) => {
-    return text.split('Alfred P2P').map((part, index) => (
+    return text.split('Alfred P2P').map((part, index) =>
       index === 1 ? (
         <>
           <a
@@ -64,22 +68,23 @@ export function ProductPage() {
           </a>
           {part}
         </>
-      ) : part
-    ));
+      ) : (
+        part
+      ),
+    );
   };
-
 
   return (
     <>
       <BackgroundAnimatedProduct />
       <section
         className={classNames(
-          'min-h-screen px-10 sm:grid sm:grid-cols-12 sm:px-8',
+          'min-h-screen px-10 lg:grid lg:grid-cols-12 lg:px-8',
           IS_ZOOM_BIGGER_THAN_100 && 'pt-20',
-          !IS_ZOOM_BIGGER_THAN_100 && 'pt-32',
+          !IS_ZOOM_BIGGER_THAN_100 && 'pt-28',
         )}
       >
-        <article className="sm:hidden">
+        <article className="lg:hidden">
           <h2 className="text-2xl leading-9 text-[#1E1E1E] font-bold dark:text-white">
             {product.name}
           </h2>
@@ -107,12 +112,12 @@ export function ProductPage() {
 
         <article
           className={classNames(
-            'hidden sm:col-span-8 sm:row-span-2 sm:flex sm:flex-col',
-            width > 843 ? 'sm:justify-center' : 'sm:justify-around',
+            'hidden lg:col-span-8 lg:row-span-2 lg:flex lg:flex-col',
+            width > 843 ? 'lg:justify-center' : 'lg:justify-around',
           )}
         >
           <div className="flex justify-around">
-            <div className="flex flex-col h-4/5 justify-between">
+            <div className="flex flex-col h-full xl:h-4/5 justify-between">
               {getVisibleImages().map((image, index) => (
                 <img
                   key={index}
@@ -120,13 +125,14 @@ export function ProductPage() {
                   alt={`Imagem do Produto ${index + 1}`}
                   className={classNames(
                     'cursor-pointer border border-solid border-black rounded-md dark:border-white',
-                    !IS_ZOOM_BIGGER_THAN_100 && 'w-28',
+                    !IS_ZOOM_BIGGER_THAN_100 && 'w-20 xl:w-28',
                     IS_ZOOM_BIGGER_THAN_100 && 'w-20',
                   )}
                   onClick={() =>
                     handleImageClick(
                       (currentIndex + index) % product.images.length,
-                    )}
+                    )
+                  }
                 />
               ))}
             </div>
@@ -136,7 +142,7 @@ export function ProductPage() {
                 alt={`Imagem do Produto Principal`}
                 className={classNames(
                   IS_ZOOM_BIGGER_THAN_100 && 'w-[550px]',
-                  !IS_ZOOM_BIGGER_THAN_100 && 'w-[800px]',
+                  !IS_ZOOM_BIGGER_THAN_100 && 'w-[400px] xl:w-[800px]',
                 )}
               />
               <div className="flex justify-center mt-4">
@@ -156,13 +162,13 @@ export function ProductPage() {
           </div>
         </article>
 
-        <article className="flex flex-col gap-y-2 sm:col-span-4">
-          <h2 className="hidden sm:block text-2xl leading-9 text-[#1E1E1E] font-bold dark:text-white">
+        <article className="flex flex-col justify-center gap-y-2 lg:col-span-4">
+          <h2 className="hidden lg:block text-2xl leading-9 text-[#1E1E1E] font-bold dark:text-white">
             {product.name}
           </h2>
           {product.available ? (
             <>
-              <div className="pt-12 sm:pt-4 flex flex-col items-start">
+              <div className="pt-12 lg:pt-4 flex flex-col items-start">
                 {product.originalPrice && (
                   <>
                     <div className="flex items-center gap-2">
@@ -174,7 +180,9 @@ export function ProductPage() {
                       </div>
                     </div>
                     <div className="flex items-baseline mt-4">
-                      <span className="text-xl leading-5 dark:text-white">R$</span>
+                      <span className="text-xl leading-5 dark:text-white">
+                        R$
+                      </span>
                       <h2 className="text-4xl font-bold leading-5 dark:text-white">
                         {product.price.toFixed(2)}
                       </h2>
@@ -196,11 +204,17 @@ export function ProductPage() {
                   width < 768 && 'cursor-pointer',
 
                   'bg-gray-900 dark:bg-[#1A202C] dark:border-gray-700',
-                  'bg-white dark:bg-[#2D3748] border-gray-300 dark:border-gray-600'
+                  'bg-white dark:bg-[#2D3748] border-gray-300 dark:border-gray-600',
                 )}
                 onClick={() => width < 768 && setIsExpanded(!isExpanded)}
               >
-                <h3 className={classNames('text-xl font-semibold', currentLang === AcceptedLanguages.pt && 'text-black dark:text-white')}>
+                <h3
+                  className={classNames(
+                    'text-xl font-semibold',
+                    currentLang === AcceptedLanguages.pt &&
+                      'text-black dark:text-white',
+                  )}
+                >
                   O que vem em nosso produto:
                 </h3>
 
@@ -212,13 +226,14 @@ export function ProductPage() {
                   className={classNames(
                     'absolute right-4 top-2 text-5xl',
                     isExpanded ? 'text-blue-500' : 'text-black',
-                    currentLang === AcceptedLanguages.pt && 'text-black dark:text-white'
+                    currentLang === AcceptedLanguages.pt &&
+                      'text-black dark:text-white',
                   )}
                 >
                   {isExpanded ? <MdExpandLess /> : <MdExpandMore />}
                 </button>
 
-                <ul className={classNames('mt-3 space-y-2', !isExpanded && 'hidden')}>
+                <ul className={classNames('pt-2', !isExpanded && 'hidden')}>
                   {product.resources.map((resource, index) => (
                     <li key={index} className="flex items-center gap-2 text-sm">
                       <MdCheck size={20} className="text-green-700" />
@@ -227,7 +242,6 @@ export function ProductPage() {
                   ))}
                 </ul>
               </div>
-
             </>
           ) : (
             <div className="flex flex-col items-center gap-y-4 text-center mt-8">
@@ -235,14 +249,16 @@ export function ProductPage() {
                 Produto Esgotado
               </h2>
               <p className="text-sm text-gray-600 dark:text-gray-300">
-                Este produto está temporariamente indisponível. Confira outros produtos incríveis em nosso catálogo!
+                Este produto está temporariamente indisponível. Confira outros
+                produtos incríveis em nosso catálogo!
               </p>
               <Link
                 to={ROUTES.products.call(currentLang || AcceptedLanguages.pt)}
                 className={classNames(
                   'w-full bg-orange-primary text-white text-center font-semibold rounded-sm transition-colors duration-300 hover:bg-orange-600 shadow-md',
-                  IS_ZOOM_BIGGER_THAN_100 && 'py-2 flex items-center justify-center',
-                  !IS_ZOOM_BIGGER_THAN_100 && 'py-4'
+                  IS_ZOOM_BIGGER_THAN_100 &&
+                    'py-2 flex items-center justify-center',
+                  !IS_ZOOM_BIGGER_THAN_100 && 'py-4',
                 )}
               >
                 Ver Mais Produtos
@@ -267,11 +283,11 @@ export function ProductPage() {
             />
           </div>
 
-          <div className="pt-4 flex gap-x-4 justify-between sm:flex-col sm:h-32 sm:gap-y-2">
+          <div className="pt-4 flex gap-x-4 justify-between lg:flex-col lg:h-32 lg:gap-y-2">
             <button
               onClick={cart.buy}
               type="button"
-              className="bg-orange-primary text-white p-2 rounded-md text-sm w-36 sm:w-full sm:gap-y-4 h-14"
+              className="bg-orange-primary text-white p-2 rounded-md text-sm w-36 lg:w-full lg:gap-y-4 h-14"
               disabled={!isAvailable}
             >
               Comprar Agora
